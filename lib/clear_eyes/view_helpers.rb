5293687964d812ff3334dea7721019dd0c944b49
Brian Dear <superacidjax@me.com>
2012-07-16 10:47:02 -0400

    Encapsulates ivars

--- lib/clear_eyes/view_helpers.rb
+++ lib/clear_eyes/view_helpers.rb
@@ -1,24 +1,24 @@
 module ClearEyes
   module ViewHelpers
-
-    def r_image( pixel_ratio )
-      insert_on = -File.extname(@image).size-1
-      image_tag(@image.insert(insert_on, "@#{pixel_ratio}x"), @options)
+    def self.included(base)
+      base.attr_accessor :image, :options
     end
 
+    def r_image( pixel_ratio )
+      insert_on = -File.extname(self.image).size-1
+      image_tag(self.image.insert(insert_on, "@#{pixel_ratio}x"), self.options)
+    end
 
     def r_image_tag(image, options = {})
-      @devicePixelRatio = cookies[:devicePixelRatio]
-      @options          = options
-      @image            = image
-
-      case @devicePixelRatio
+      self.options      = options
+      self.image        = image
+      case cookies[:devicePixelRatio]
         when '2'
           r_image 2
         when '1.5'
           r_image 2
         else
-          image_tag(@image, @options)
+          image_tag(self.image, self.options)
        end
      end
 
