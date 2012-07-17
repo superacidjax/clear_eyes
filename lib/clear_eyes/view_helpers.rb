5b7795c1e6dda16b9ba62a59631b5bdfe38873e4
Brian Dear <superacidjax@me.com>
2012-07-17 10:13:54 -0400

    improves encapsulation

--- lib/clear_eyes/view_helpers.rb
+++ lib/clear_eyes/view_helpers.rb
@@ -1,13 +1,14 @@
 module ClearEyes
   module ViewHelpers
     def self.included(base)
-      attr_accessor :image, :options
+      base.send(:attr_accessor, :image, :options)
     end
 
     def r_image( pixel_ratio )
       insert_on = -File.extname(self.image).size-1
       image_tag(self.image.insert(insert_on, "@#{pixel_ratio}x"), self.options)
     end
+
 
     def r_image_tag(image, options = {})
       self.options      = options
